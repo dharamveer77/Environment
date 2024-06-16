f72b5b135f168316393765069efaf7b12889ab2d
unknown <77dharamveer77@gmai.com>
2024-06-16 18:43:09 +0530

    loader updated

---
+++ header.php
@@ -0,0 +1,20 @@
+<nav>
+      
+      <div class="logo">
+        <img src="logo1.jpg" alt="" />
+      </div>
+    
+    
+      <input type="checkbox" id="click" />
+      <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <ul>
+        <li><a href="registration.html">Register</a></li>
+        <li><a href="#progress">Progress</a></li>
+        <li><a href="#about us">About Us</a></li>
+        <li><a href="#gallary">Gallary</a></li>
+        <li><a href="contact.html">Contact Us</a></li>
+        <li><a class="active" href="login.html">Login</a></li>
+      </ul>
+    </nav>
